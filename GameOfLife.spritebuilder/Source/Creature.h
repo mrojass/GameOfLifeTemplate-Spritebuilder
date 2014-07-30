33c331857ccedc3579010df3c7057d8248d830f7
Michelle <mrojass1028@gmail.com>
2014-07-30 00:41:06 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Creature.h
+++ GameOfLife.spritebuilder/Source/Creature.h
@@ -10,4 +10,12 @@
 
 @interface Creature : CCSprite
 
+// stores the current state of the creature
+@property (nonatomic, assign) BOOL isAlive;
+
+// stores the amount of living neighbors
+@property (nonatomic, assign) NSInteger livingNeighbors;
+
+-(id)initCreature;
+
 @end
