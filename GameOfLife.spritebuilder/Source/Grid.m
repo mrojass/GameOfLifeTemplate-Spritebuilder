177cad0feac6a731d11f65e1b8f4295d1ba6f398
Michelle <mrojass1028@gmail.com>
2014-07-30 18:20:44 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -157,7 +157,7 @@ static const int GRID_COLUMNS = 10;
 {
     for (int i = 0; i < [_gridArray count]; i++)
     {
-        for (int j = 0; j < [_gridArray[i] count]; i++)
+        for (int j = 0; j < [_gridArray[i] count]; j++)
         {
             Creature *currentCreature = _gridArray[i][j];
             
